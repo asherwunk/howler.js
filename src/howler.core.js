0d496a9878c154816d29532797d9019552736f52
Asher Wolfstein <asherwunk@gmail.com>
2017-06-18 23:12:31 +0000

    Added contact information

--- src/howler.core.js
+++ src/howler.core.js
@@ -10,6 +10,7 @@
  *  Modified by: Asher Wolfstein (asherwunk@gmail.com) June 18th, 2017
  *               For more information see my blog at http://wunk.me/
  *               Also the specific URL: http://wunk.me/programming-projects/pygjs
+ *
  *  
  */
 
